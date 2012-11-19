b6ea2e46abd624b8b6d2ff6845fc940b7e1b027a
Félix Perreault <perreault.felix@gmail.com>
2012-11-19 23:33:45 +0100

    A software to start working on projects using the kinect

---
+++ KinectTesterForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace KinectTester
+{
+    partial class KinectTesterForm
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+      this.buttonStart = new System.Windows.Forms.Button();
+      this.pictureBoxVideo = new System.Windows.Forms.PictureBox();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBoxVideo)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // buttonStart
+      // 
+      this.buttonStart.Location = new System.Drawing.Point(26, 12);
+      this.buttonStart.Name = "buttonStart";
+      this.buttonStart.Size = new System.Drawing.Size(173, 56);
+      this.buttonStart.TabIndex = 0;
+      this.buttonStart.Text = "Start";
+      this.buttonStart.UseVisualStyleBackColor = true;
+      this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+      // 
+      // pictureBoxVideo
+      // 
+      this.pictureBoxVideo.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+      this.pictureBoxVideo.Location = new System.Drawing.Point(26, 73);
+      this.pictureBoxVideo.Name = "pictureBoxVideo";
+      this.pictureBoxVideo.Size = new System.Drawing.Size(640, 480);
+      this.pictureBoxVideo.TabIndex = 1;
+      this.pictureBoxVideo.TabStop = false;
+      this.pictureBoxVideo.Click += new System.EventHandler(this.pictureBoxVideo_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(689, 573);
+      this.Controls.Add(this.pictureBoxVideo);
+      this.Controls.Add(this.buttonStart);
+      this.Name = "Form1";
+      this.Text = "Kinect Tester";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBoxVideo)).EndInit();
+      this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.PictureBox pictureBoxVideo;
+    }
+}
+
---
+++ KinectTesterForm.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Windows.Forms;
+using KinectTester.Properties;
+using Microsoft.Research.Kinect.Nui;
+using System.Runtime.InteropServices;
+
+namespace KinectTester
+{
+  public partial class KinectTesterForm : Form
+  {
+    private Runtime _nui;
+
+    public KinectTesterForm()
+    {
+      InitializeComponent();
+    }
+
+    private void buttonStart_Click(object sender, EventArgs e)
+    {
+      _nui = Runtime.Kinects.FirstOrDefault();
+      if (_nui != null)
+      {
+        if (buttonStart.Text == Resources.Form1_buttonStart_Click_Start)
+        {
+          buttonStart.Text = Resources.Form1_buttonStart_Click_Stop;
+
+          _nui.Initialize(RuntimeOptions.UseColor);
+          _nui.VideoStream.Open(ImageStreamType.Video, 2, ImageResolution.Resolution640x480, ImageType.Color);
+
+          _nui.VideoFrameReady += FrameReady;
+        }
+        else if (buttonStart.Text == Resources.Form1_buttonStart_Click_Stop)
+        {
+          buttonStart.Text = Resources.Form1_buttonStart_Click_Start;
+          _nui.Uninitialize();
+        }
+      }
+      else
+      {
+        var dr = MessageBox.Show(Resources.Form1_buttonStart_Click_Please_connect_a_Microsoft_Kinect_to_the_computer,
+                                 Resources.Form1_buttonStart_Click_Error, MessageBoxButtons.RetryCancel);
+        if (dr == DialogResult.Retry)
+        {
+          buttonStart_Click(sender, e);
+        }
+      }
+    }
+
+    private void FrameReady(object sender, ImageFrameReadyEventArgs e)
+    {
+      var pImage = e.ImageFrame.Image;
+      var bmap = PImageToBitmap(pImage);
+      pictureBoxVideo.Image = bmap;
+    }
+    
+    private static Bitmap PImageToBitmap(PlanarImage pImage)
+    {
+      var bmap = new Bitmap(pImage.Width, pImage.Height, PixelFormat.Format32bppRgb);
+      var bmapdata = bmap.LockBits(new Rectangle(0, 0, pImage.Width, pImage.Height), ImageLockMode.WriteOnly,
+                                   bmap.PixelFormat);
+      var ptr = bmapdata.Scan0;
+      Marshal.Copy(pImage.Bits, 0, ptr, pImage.Width * pImage.BytesPerPixel * pImage.Height);
+      
+      bmap.UnlockBits(bmapdata);
+      return bmap;
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      //
+    }
+
+    private void pictureBoxVideo_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void button(object sender, EventArgs e)
+    {
+      var vector = new Vector();
+    }
+  }
+}
